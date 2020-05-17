6a6bf6306fa104df5601c2945b60c80e0c0f3ef6
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-17 18:55:13 +0530

    http calls test setup

--- src/app/modules/books/services/book.service.spec.ts
+++ src/app/modules/books/services/book.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BookService } from './book.service';
 
-describe('BookService', () => {
+fdescribe('BookService', () => {
   let service: BookService;
 
   beforeEach(() => {
@@ -14,5 +14,25 @@ describe('BookService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch all books - GET /books', () => {
+    pending();
+  });
+
+  it('should get one book - GET /books/12', () => {
+    pending();
+  });
+
+  it('should add one book - POST /books', () => {
+    pending();
+  });
+
+  it('should update one book - PUT /books/12', () => {
+    pending();
+  });
+
+  it('should remove one book - DELETE /books/12', () => {
+    pending();
   });
 });
