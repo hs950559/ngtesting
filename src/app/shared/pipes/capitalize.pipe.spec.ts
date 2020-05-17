053b2d6e061b0013abd9da4da0a89bf00a4014e2
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-17 18:46:54 +0530

    shallow pipe test added

--- src/app/shared/pipes/capitalize.pipe.spec.ts
+++ src/app/shared/pipes/capitalize.pipe.spec.ts
@@ -1,4 +1,6 @@
 import { CapitalizePipe } from './capitalize.pipe';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('CapitalizePipe', () => {
   let pipe: CapitalizePipe;
@@ -16,5 +18,46 @@ describe('CapitalizePipe', () => {
 
     const inputStr1 = 'hemant';
     expect(pipe.transform(inputStr1)).toEqual('Hemant');
+  });
+
+  describe('Shallow Capitalize test', () => {
+    @Component({
+      template: `Name: {{ name | capitalize }}`,
+    })
+    class TestComponent {
+      name = 'hemant Kumar singh';
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [CapitalizePipe, TestComponent],
+      });
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+      fixture.detectChanges();
+    });
+
+    it('component template should have capitalized 1st char of each word', () => {
+      expect(el.textContent).toContain('Name: Hemant Kumar Singh');
+
+      component.name = 'mr. bean';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Name: Mr. Bean');
+    });
+
+    // it('should use the default argument when not supplied', () => {
+    //   expect(el.textContent).toContain('Size: 117.74MB');
+    // });
+
+    // it('should override the default argument when supplied', () => {
+    //   component.arg1 = 'soammcase';
+    //   fixture.detectChanges();
+    //   expect(el.textContent).toContain('Size: 117.74myExt');
+    // });
   });
 });
