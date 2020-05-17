43f47850cce4971785a6a715865eb020cb8d9c74
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-17 18:30:22 +0530

    capitalize pipe isolate test added

--- src/app/shared/pipes/capitalize.pipe.spec.ts
+++ src/app/shared/pipes/capitalize.pipe.spec.ts
@@ -1,8 +1,20 @@
 import { CapitalizePipe } from './capitalize.pipe';
 
 describe('CapitalizePipe', () => {
+  let pipe: CapitalizePipe;
+  beforeEach(() => {
+    pipe = new CapitalizePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new CapitalizePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should capitalize 1st char of each word', () => {
+    const inputStr = 'hemant Kumar singh';
+    expect(pipe.transform(inputStr)).toEqual('Hemant Kumar Singh');
+
+    const inputStr1 = 'hemant';
+    expect(pipe.transform(inputStr1)).toEqual('Hemant');
   });
 });
